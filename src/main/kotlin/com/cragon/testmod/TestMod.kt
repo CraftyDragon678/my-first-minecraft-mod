d2608aa3f0a13607df05a7b424673d008153e896
CraftyDragon678 <craftydragon678@gmail.com>
2020-02-09 12:30:53 +0900

    Remove comments

--- src/main/kotlin/com/cragon/testmod/TestMod.kt
+++ src/main/kotlin/com/cragon/testmod/TestMod.kt
@@ -2,12 +2,7 @@ package com.cragon.testmod
 
 // For support join https://discord.gg/v6v4pMv
 
-@Suppress("unused")
 fun init() {
-    // This code runs as soon as Minecraft is in a mod-load-ready state.
-    // However, some things (like resources) may still be uninitialized.
-    // Proceed with mild caution.
-
     println("Hello Fabric world!")
 }
 
