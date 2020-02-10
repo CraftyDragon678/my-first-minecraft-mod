63549c75125d222f1e99577c14b8025e34bf86a1
CraftyDragon678 <craftydragon678@gmail.com>
2020-02-10 13:40:21 +0900

    통커밋

---
+++ src/main/kotlin/com/cragon/testmod/HeadFeatureRendererMixin.kt
@@ -0,0 +1,108 @@
+package com.cragon.testmod
+
+import com.mojang.authlib.GameProfile
+import net.minecraft.block.AbstractSkullBlock
+import net.minecraft.block.entity.SkullBlockEntity
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.block.entity.SkullBlockEntityRenderer
+import net.minecraft.client.render.entity.feature.FeatureRenderer
+import net.minecraft.client.render.entity.feature.FeatureRendererContext
+import net.minecraft.client.render.entity.model.EntityModel
+import net.minecraft.client.render.entity.model.ModelWithHead
+import net.minecraft.client.render.model.json.ModelTransformation
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.client.util.math.Vector3f
+import net.minecraft.entity.EquipmentSlot
+import net.minecraft.entity.LivingEntity
+import net.minecraft.entity.mob.ZombieVillagerEntity
+import net.minecraft.entity.passive.VillagerEntity
+import net.minecraft.item.ArmorItem
+import net.minecraft.item.BlockItem
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.nbt.NbtHelper
+import net.minecraft.util.math.Direction
+import org.apache.commons.lang3.StringUtils
+import java.util.*
+
+
+class HeadFeatureRendererMixin<T : LivingEntity?, M>(context: FeatureRendererContext<T, M>?) :
+    FeatureRenderer<T, M>(context) where M : EntityModel<T>?, M : ModelWithHead? {
+    override fun render(
+        matrixStack: MatrixStack,
+        vertexConsumerProvider: VertexConsumerProvider,
+        i: Int,
+        livingEntity: T,
+        f: Float,
+        g: Float,
+        h: Float,
+        j: Float,
+        k: Float,
+        l: Float
+    ) {
+        val itemStack = livingEntity!!.getEquippedStack(EquipmentSlot.HEAD)
+        if (!itemStack.isEmpty) {
+            val item = itemStack.item
+            matrixStack.push()
+            val bl = livingEntity is VillagerEntity || livingEntity is ZombieVillagerEntity
+            var p: Float
+            if (livingEntity.isBaby && livingEntity !is VillagerEntity) {
+                p = 2.0f
+                val n = 1.4f
+                matrixStack.translate(0.0, 0.03125, 0.0)
+                matrixStack.scale(0.7f, 0.7f, 0.7f)
+                matrixStack.translate(0.0, 1.0, 0.0)
+            }
+            (this.contextModel as ModelWithHead).head.rotate(matrixStack)
+            if (item is BlockItem && item.block is AbstractSkullBlock) {
+                p = 1.1875f
+                matrixStack.scale(1.1875f, -1.1875f, -1.1875f)
+                if (bl) {
+                    matrixStack.translate(0.0, 0.0625, 0.0)
+                }
+                var gameProfile: GameProfile? = null
+                if (itemStack.hasTag()) {
+                    val compoundTag = itemStack.tag
+                    if (compoundTag!!.contains("SkullOwner", 10)) {
+                        gameProfile = NbtHelper.toGameProfile(compoundTag.getCompound("SkullOwner"))
+                    } else if (compoundTag.contains("SkullOwner", 8)) {
+                        val string = compoundTag.getString("SkullOwner")
+                        if (!StringUtils.isBlank(string)) {
+                            gameProfile = SkullBlockEntity.loadProperties(GameProfile(null as UUID?, string))
+                            compoundTag.put("SkullOwner", NbtHelper.fromGameProfile(CompoundTag(), gameProfile))
+                        }
+                    }
+                }
+                matrixStack.translate(-0.5, 0.0, -0.5)
+                SkullBlockEntityRenderer.render(
+                    null as Direction?,
+                    180.0f,
+                    (item.block as AbstractSkullBlock).skullType,
+                    gameProfile,
+                    f,
+                    matrixStack,
+                    vertexConsumerProvider,
+                    i
+                )
+            } else if (item !is ArmorItem || item.slotType != EquipmentSlot.HEAD) {
+                p = 0.625f
+                matrixStack.translate(0.0, -0.25, 0.0)
+                matrixStack.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(180.0f))
+                matrixStack.scale(0.625f, -0.625f, -0.625f)
+                if (bl) {
+                    matrixStack.translate(0.0, 0.1875, 0.0)
+                }
+                MinecraftClient.getInstance().heldItemRenderer.renderItem(
+                    livingEntity,
+                    itemStack,
+                    ModelTransformation.Mode.HEAD,
+                    false,
+                    matrixStack,
+                    vertexConsumerProvider,
+                    i
+                )
+            }
+            matrixStack.pop()
+        }
+    }
+}
--- src/main/kotlin/com/cragon/testmod/mixin/ArmorStandEntityRendererMixin.kt
+++ src/main/kotlin/com/cragon/testmod/mixin/ArmorStandEntityRendererMixin.kt
@@ -1,46 +1,17 @@
 package com.cragon.testmod.mixin
 
-import net.minecraft.client.MinecraftClient
-import net.minecraft.client.render.VertexConsumerProvider
+import com.cragon.testmod.HeadFeatureRendererMixin
 import net.minecraft.client.render.entity.ArmorStandEntityRenderer
 import net.minecraft.client.render.entity.EntityRenderDispatcher
-import net.minecraft.client.render.entity.LivingEntityRenderer
-import net.minecraft.client.render.entity.feature.HeadFeatureRenderer
-import net.minecraft.client.render.entity.model.ArmorStandArmorEntityModel
-import net.minecraft.client.render.entity.model.ArmorStandEntityModel
-import net.minecraft.client.util.math.MatrixStack
-import net.minecraft.entity.decoration.ArmorStandEntity
-import net.minecraft.text.Text
-import net.minecraft.util.Identifier
 import org.spongepowered.asm.mixin.Mixin
 
 @Suppress("unused")
 @Mixin(ArmorStandEntityRenderer::class)
 class ArmorStandEntityRendererMixin(entityRenderDispatcher: EntityRenderDispatcher?) :
-    LivingEntityRenderer<ArmorStandEntity, ArmorStandArmorEntityModel>(
-        entityRenderDispatcher,
-        ArmorStandEntityModel(),
-        0F
-    ) {
-    val SKIN = Identifier("textures/entity/armorstand/wood.png")
+    ArmorStandEntityRenderer(entityRenderDispatcher) {
 
     init {
-        this.addFeature(HeadFeatureRenderer(this))
-    }
-
-    override fun render(
-        livingEntity: ArmorStandEntity?,
-        f: Float,
-        g: Float,
-        matrixStack: MatrixStack?,
-        vertexConsumerProvider: VertexConsumerProvider?,
-        i: Int
-    ) {
-        MinecraftClient.getInstance().player!!.sendMessage(Text.Serializer.fromJson("""{"text":"debug"}"""))
-        super.render(livingEntity, f, g, matrixStack, vertexConsumerProvider, i)
-    }
-
-    override fun getTexture(entity: ArmorStandEntity?): Identifier {
-        return SKIN
+        this.features.removeAt(3)
+        this.addFeature(HeadFeatureRendererMixin(this))
     }
 }
